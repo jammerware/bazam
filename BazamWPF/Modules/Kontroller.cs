f384bade73494dc37733455efb9385b1e9fb5751
Jammer <ben.s.stein@gmail.com>
2013-11-17 17:15:13 -0600

    Replaced a crappy module with "Kontroller," a module to help people work with WPF controls. Added the "Blur" method.

---
+++ BazamWPF/Modules/Kontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BazamWPF.Modules
+{
+    public static class Kontroller
+    {
+        public static void Blur(FrameworkElement element)
+        {
+            FrameworkElement parent = (FrameworkElement)element.Parent;
+            while (parent != null && parent is IInputElement && !((IInputElement)parent).Focusable) {
+                parent = (FrameworkElement)parent.Parent;
+            }
+            DependencyObject scope = FocusManager.GetFocusScope(element);
+            FocusManager.SetFocusedElement(scope, parent as IInputElement);
+        }
+
+        public static void Click(Button button)
+        {
+            ButtonAutomationPeer peer = new ButtonAutomationPeer(button);
+            IInvokeProvider provider = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+            provider.Invoke();
+        }
+    }
+}
