6a583e4d9252bb5623948a7cd0cf418b7a1ddbbb
Ben Stein <ben.s.stein@gmail.com>
2014-11-19 10:18:37 -0600

    Added WindowHelper class to BazamWPF.

---
+++ BazamWPF/UIHelpers/WindowHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace BazamWPF.UIHelpers
+{
+    public static sealed class WindowHelper
+    {
+        public static bool IsWindowOpen<T>(string name = "") where T : Window
+        {
+            return Application.Current.Windows.OfType<T>().Any(w => (string.IsNullOrEmpty(name) || w.Name.Equals(name)));
+        }
+    }
+}
