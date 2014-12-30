1410fdf3755cf63b9b43254add408714d2508fad
Ben Stein <ben.s.stein@gmail.com>
2014-12-30 15:52:52 -0600

    Added MonitorLizard module for working with displays (monitors).

---
+++ Bazam/Modules/MonitorLizard.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bazam.Modules
+{
+    public static class MonitorLizard
+    {
+        public static Screen[] GetDisplays()
+        {
+            return Screen.AllScreens;
+        }
+
+        public static Point GetMidpointForDisplay(Screen screen)
+        {
+            Rectangle screenBounds = screen.Bounds;
+            return new Point((screenBounds.Right - screenBounds.Left) / 2, (screenBounds.Bottom - screenBounds.Top) / 2);
+        }
+    }
+}
