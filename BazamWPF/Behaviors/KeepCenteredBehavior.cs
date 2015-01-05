844197517b4556c0dd9ca84831028b010eefd990
Ben Stein <ben.s.stein@gmail.com>
2015-01-05 15:56:38 -0600

    Corrected bugs with KeepCenteredBehavior.

--- BazamWPF/Behaviors/KeepCenteredBehavior.cs
+++ BazamWPF/Behaviors/KeepCenteredBehavior.cs
@@ -10,21 +10,23 @@ namespace BazamWPF.Behaviors
     public class KeepCenteredBehavior : Behavior<Window>
     {
         private bool _DoShit = true;
-        private Point _ScreenMidpoint;
+
+        private Point GetMidPoint()
+        {
+            Screen screen = Screen.FromHandle(new WindowInteropHelper(AssociatedObject).Handle);
+            double screenHeight = screen.WorkingArea.Height;
+            double screenWidth = screen.WorkingArea.Width;
+            return new Point(screenWidth / 2, screen.Bounds.Y + (screenHeight / 2));
+        }
 
         protected override void OnAttached()
         {
-            var screen = Screen.FromHandle(new WindowInteropHelper(AssociatedObject).Handle);
-            double screenHeight = screen.WorkingArea.Height;
-            double screenWidth = screen.WorkingArea.Width;
-            _ScreenMidpoint = new Point(screenWidth / 2, screenHeight / 2);
-
             AssociatedObject.SizeChanged += (omg, soAwesome) => {
                 if (_DoShit && AssociatedObject.DesiredSize.Height > 0) {
                     _DoShit = false;
-                    
-                    double newLeft = _ScreenMidpoint.X - (AssociatedObject.DesiredSize.Width / 2);
-                    double newTop = _ScreenMidpoint.Y - (AssociatedObject.DesiredSize.Height / 2);
+
+                    Point midpoint = GetMidPoint();
+                    double newTop = midpoint.Y - (AssociatedObject.DesiredSize.Height / 2);
 
                     Storyboard sb = new Storyboard();
                     DoubleAnimation anim = new DoubleAnimation(AssociatedObject.Top, newTop, new Duration(TimeSpan.FromMilliseconds(150)));
@@ -35,6 +37,7 @@ namespace BazamWPF.Behaviors
                     Storyboard.SetTarget(anim, AssociatedObject);
                     sb.Children.Add(anim);
                     sb.Completed += (fuck, it) => {
+                        AssociatedObject.Top = newTop;
                         _DoShit = true;
                         sb.Stop();
                     };
