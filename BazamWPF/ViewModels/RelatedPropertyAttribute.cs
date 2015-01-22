c8b6f46c51b0d4e6b801b8bfdb9a7738d090fece
Ben Stein <ben.s.stein@gmail.com>
2015-01-22 10:32:31 -0600

    Updated assembly versions and added improved ViewModelBase design.

---
+++ BazamWPF/ViewModels/RelatedPropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BazamWPF.ViewModels
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class RelatedPropertyAttribute : Attribute
+    {
+        public string RelatedPropertyName { get; set; }
+
+        public RelatedPropertyAttribute(string relatedPropertyName)
+        {
+            this.RelatedPropertyName = relatedPropertyName;
+        }
+    }
+}
