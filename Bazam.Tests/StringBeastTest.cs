9d67db6d1b7fd1314b630367f43b528b52440a2e
Ben Stein <ben.s.stein@gmail.com>
2015-06-05 15:07:21 -0500

    Added string compression to StringBeast.

--- Bazam.Tests/StringBeastTest.cs
+++ Bazam.Tests/StringBeastTest.cs
@@ -14,41 +14,19 @@ namespace Bazam.Tests
             set { testContextInstance = value; }
         }
 
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
         [TestMethod()]
         [TestCategory("Offline")]
         public void CapitalizeTest()
         {
             Assert.AreEqual("Sergei Temkin sucks.", StringBeast.Capitalize("sergei Temkin sucks."));
+        }
+        
+        [TestMethod()]
+        [TestCategory("Offline")]
+        public void CompressionWorks()
+        {
+            string testString = "This compression crap totally works.";
+            Assert.AreEqual(testString, StringBeast.Decompress(StringBeast.Compress(testString)));
         }
 
         [TestMethod()]
