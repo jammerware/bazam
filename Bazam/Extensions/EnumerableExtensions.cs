bcaa480109fea156683eacee27138d0f7851512f
Ben Stein <ben.s.stein@gmail.com>
2015-07-02 11:08:48 -0500

    Added EnumerableExtensions.

---
+++ Bazam/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bazam.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static T Random<T>(this IEnumerable<T> helper)
+        {
+            T[] array = helper.ToArray();
+            return array[new Random().Next(array.Length)];
+        }
+    }
+}
