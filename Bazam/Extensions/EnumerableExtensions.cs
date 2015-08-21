f32d25a57547b586c94dac983563ec51af1edb03
Ben Stein <ben.s.stein@gmail.com>
2015-08-21 15:40:37 -0500

    Removed the Listless module and refactored it into the IEnumerable extensions. Probably should do that with other modules too...

--- Bazam/Extensions/EnumerableExtensions.cs
+++ Bazam/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +7,25 @@ namespace Bazam.Extensions
 {
     public static class EnumerableExtensions
     {
+        public static string Concatenate(this IEnumerable helper)
+        {
+            return Concatenate(helper, ",");
+        }
+
+        public static string Concatenate(this IEnumerable helper, string delimiter)
+        {
+            string retVal = string.Empty;
+            foreach (object item in helper) {
+                retVal += item.ToString() + delimiter;
+            }
+
+            if (retVal == string.Empty) {
+                return retVal;
+            }
+
+            return retVal.Substring(0, retVal.Length - delimiter.Length);
+        }
+
         public static T Random<T>(this IEnumerable<T> helper)
         {
             T[] array = helper.ToArray();
